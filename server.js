f125af88fe4f19cf2101e947aa75c8cb258bab58
System <none>
2017-08-09 23:29:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
