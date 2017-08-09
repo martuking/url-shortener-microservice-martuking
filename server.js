1ebb744a60b44a1e5600baded1ae54c3f8fd2e1e
System <none>
2017-08-09 23:18:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,28 +1,10 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-var idcounter=1;
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.use('/:original',function(request,response){
-  var original = 'https:/'+request.originalUrl;
-  var short = 'https://url-shortener-microservice-martuking.glitch.me/'+idcounter;
-  idcounter++;
-  response.json({original_url:original, short_url:short});
-});
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
