f1a95b395d3c5ce3400979b5fa585989b095a39c
System <none>
2017-08-10 16:47:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+var dbStructure = require('/db_structure');
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.get('/new/:urlToShorten',function(request,response,next){
+app.get('/:urlToShorten',function(request,response,next){
   var url = request.params.urlToShorten;
-  response.send("esta funcionando y esta es tu url:"+url);
+  response.json({original_url:url,short_url:"url corta"});
 });
 
 
