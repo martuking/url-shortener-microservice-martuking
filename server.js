420bd58fd47d16f2ea3b919e692b866e142d27dd
System <none>
2017-08-10 16:25:32 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+app.get('/new/:');
 
 
 var listener = app.listen(process.env.PORT, function () {
